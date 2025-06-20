598a4974d165e9b673b09c66ccb9efcf3682a9a5
Rachit Khandelwal <rachitkhandelwal21@gmail.com>
2025-06-20 17:32:08 +0530

    Updated budget component and fixed alert tys

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
 
   return (
    <Routes>
+     <Route path="/" element={<Signin/>}/>
     <Route path="/signup" element={<Signup/>}/>
     <Route path="/signin" element={<Signin/>}/>
     <Route path="/dashboard" element={<PrivateRoute><DashBoard/></PrivateRoute>}/>
