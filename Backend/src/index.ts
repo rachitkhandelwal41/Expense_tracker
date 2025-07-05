afaea67303e1e8a640053d2ca9a7366d228bd23f
Rachit Khandelwal <160627704+rachitkhandelwal41@users.noreply.github.com>
2025-07-05 13:27:14 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,11 +6,7 @@ const app = express();
 
 
 // Handle preflight requests
-app.options("*", cors({
-  origin: ["https://expense-tracker2-two.vercel.app", "http://localhost:5173"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-}));
+app.use(cors());
 
 app.use(express.json());
 
